21ddac59a49d41a2e93ea541f2a08a1bc043e880
ShivaramMorigala <shivaramiitmb085@gmail.com>
2024-11-30 15:00:27 +0530

    deploying frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  build :{
+    outDir: path.resolve(__dirname, '../dist'), // Adjust this path as needed
+  },
   plugins: [react()],
 })
